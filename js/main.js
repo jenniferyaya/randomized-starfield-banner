42287dd511016fbb8337eb9d994f5b62f80f45c4
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-04-04 18:09:35 -0400

    Trying to get the randomizer to set in

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var $addStars;
 var $width = $banner.outerWidth();
 var $height = $banner.outerHeight();
 
+for (i = 0; i < 75; i++) {
+  $addStars = $('<div>');
+  $addStars.addClass('star');
+  $addStars.css('left', Math.random() * 1000);
+  $addStars.css('top', Math.random() * 1000);
+  $addStars.css('opacity', Math.random() * 10);
+
+
+  $banner.append($addStars);
+};
