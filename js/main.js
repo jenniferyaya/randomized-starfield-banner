7e9430cd268e4b73a89cb25130c890f1b3b0fcd3
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-04-05 11:56:15 -0400

    Added animation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-var $banner = $('.banner');
-var $addStars;
-var $width = $banner.outerWidth();
-var $height = $banner.outerHeight();
+var $body = $('.banner');
+var $star;
+var width = $body.outerWidth();
+var height = $body.outerHeight();
 
 for (i = 0; i < 75; i++) {
-  $addStars = $('<div>');
-  $addStars.addClass('star');
-  $addStars.css('left', Math.random() * 1000);
-  $addStars.css('top', Math.random() * 1000);
-  $addStars.css('opacity', Math.random() * 10);
-
-
-  $banner.append($addStars);
-};
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+    top: Math.random() * height,
+    left: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+  });
+  $body.append($star);
+}
